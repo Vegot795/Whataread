d3d442d584482c532189286d3e9c98bd1f8785fc
Vegot795 <167176338+Vegot795@users.noreply.github.com>
2024-07-07 19:26:17 +0200

    add buttons

--- AppMod.py
+++ AppMod.py
@@ -43,10 +43,27 @@ class App(ctk.CTk):
         for index, book in enumerate(current_books):
             book_frame = ctk.CTkFrame(self.book_list_frame)
             book_frame.pack(fill='x', pady=5, padx=5)
+            book_frame.columnconfigure(0, weight=1)
+            book_frame.columnconfigure(1, weight=0)
 
-            book_title_label = ctk.CTkLabel(book_frame, text=f"Title: {book['title']}")
+            info_frame = ctk.CTkFrame(book_frame)
+            info_frame.grid(column=0, row=0, sticky='ew', padx=5)
+
+            button_frame = ctk.CTkFrame(book_frame)
+            button_frame.grid(column=1, row=0, sticky='ew', padx=5)
+
+            book_title_label = ctk.CTkLabel(info_frame, text=f"Title: {book['title']}")
             book_title_label.pack(fill='x', expand=False)
-            book_author_label = ctk.CTkLabel(book_frame, text=f"Author: {book['author']}")
+            book_author_label = ctk.CTkLabel(info_frame, text=f"Author: {book['author']}")
             book_author_label.pack(fill='x', expand=False)
-            book_publisher_label = ctk.CTkLabel(book_frame, text=f"Publisher: {book['publisher']}")
+            book_publisher_label = ctk.CTkLabel(info_frame, text=f"Publisher: {book['publisher']}")
             book_publisher_label.pack(fill='x', expand=False)
+
+            move_up_button = ctk.CTkButton(button_frame, width=5, height=5, text="UP")
+            move_up_button.grid(row=0, padx=5, pady=5)
+
+            edit_button = ctk.CTkButton(button_frame, width=5, height=5, text="Edit")
+            edit_button.grid(row=1, padx=5, pady=5)
+
+            move_down_button = ctk.CTkButton(button_frame, width=5, height=5, text="down")
+            move_down_button.grid(row=2, padx=5, pady=5)
