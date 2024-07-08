62c1e536f303f1224cd4cb124bcc2a502bd040ec
Vegot795 <davies.ogunruku@outlook.com>
2024-07-08 20:58:14 +0200

    ready to go

--- test/test_AppMod.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import unittest.mock as mock
-import App.AppMod as AppMod
-
-@mock.patch("App.AppMod.delete_book")
-def test_delete_book(mock_delete_book):
-    mock_delete_book.return_value = "Book deleted"
-    
